4477b3f8b467ef2bb038ddcc56fb0c5ea8db833b
snk-git-hub <shivanandu.dev@gmail.com>
2025-06-02 03:44:35 +0530

    signin and signup express endpoint  created

--- apps/index.js
+++ apps/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { message } = require("statuses");
 const app = express();
-ppid.use(express.json());
+const db = require("./db");
+const bcrypt = require("bcrypt");
+app.use(express.json());
+
 app.get('/', (req, res) => {
 
     res.send("Course selling app backend is listening ")
@@ -8,48 +12,79 @@ app.get('/', (req, res) => {
 
 //                      user
 
-function userAuth(req,res,next){
-    
+function userAuth(req, res, next) {
     next()
 }
+app.post('/api/v1/user/login', userAuth, async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await db.User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" })
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid email or password" })
+        }
+        const token = jwt.sign(
+            { id: user._id, username: user.username },
+            process.env.SECRET_KEY, { expiresIn: '1h' }
+        )
+        res.json({ token })
+    } catch (error) {
+        res.status(5000).json({ message: "invalid ", error })
+    }
+})
+app.post('/api/v1/user/signin', userAuth, async(req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await db.User.findOne({ email })
+        if (user) {
+            return res.status(400).json({ message: "Email already exist" })
+        }
+        const hashpassword= await bcrypt.hash(password,10)
+        const newUser = db.insert({ name, email, hashpassword })
+        res.json({ message: "User created successfully" })
 
-app.post('/api/v1/user/login',userAuth, (req, res) => {
+    } catch (err) {
+        res.status(500).json({ message: "Error creating user", error: err })
+
+    }
+
+
+}
+)
+app.get("/api/v1/user/seecourse", userAuth, (req, res) => {
 
 })
-app.post('/api/v1/user/signin', userAuth,(req, res) => {
-
-})
-app.get("/api/v1/user/seecourse", userAuth,(req, res) => {
-
-})
-app.post('/api/v1/user/purchsecourse', userAuth,(req, res) => {
+app.post('/api/v1/user/purchsecourse', userAuth, (req, res) => {
 
 })
 
 //                       admin
 
-function adminAuth(req,res,next){
+function adminAuth(req, res, next) {
 
-next()
+    next()
 }
-app.post("/api/v1/admin/login",adminAuth,(req,res)=>{
+app.post("/api/v1/admin/login", adminAuth, (req, res) => {
 
 })
-app.post("/api/v1/admin/signup",adminAuth,(req,res)=>{
-    
+app.post("/api/v1/admin/signup", adminAuth, (req, res) => {
+
 })
 
-app.post("/api/v1/admin/create",adminAuth,(req,res)=>{
-    
+app.post("/api/v1/admin/create", adminAuth, (req, res) => {
+
 })
 
-app.post("/api/v1/admin/add/course",adminAuth,(req,res)=>{
-    
+app.post("/api/v1/admin/add/course", adminAuth, (req, res) => {
+
 })
 
 
-app.delete("/api/v1/admin/delete/:courseId",adminAuth,(req,res)=>{
-    
+app.delete("/api/v1/admin/delete/:courseId", adminAuth, (req, res) => {
+
 })
 
 
